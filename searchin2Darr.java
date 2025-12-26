d6c77dc459b92dd4740265f8c58ca2a287bc2d4e
Shreyasi53 <shreyasidutta53@gmail.com>
2025-12-26 10:40:34 +0530

    BS in 2D array

--- searchin2Darr.java
+++ searchin2Darr.java
@@ -1,5 +1,5 @@
 import java.util.Arrays;
-public class BSin2Darr {
+public class searchin2Darr {
     public static void main(String[] args) {
             int[][] arr={
                     {10,20,30,40},
