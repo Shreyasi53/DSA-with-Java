30bf1f97f9543373050cb4c8f1fd836b004a67a6
Shreyasi53 <shreyasidutta53@gmail.com>
2026-01-13 10:36:50 +0530

    Cyclic sort

---
+++ FindDuplicate.java
@@ -0,0 +1,34 @@
+//LC 287
+
+import java.util.Arrays;
+
+public class FindDuplicate {
+    public static void main(String[] args) {
+        int[] arr = {3,5,2,2,1,4};
+        findDuplicate(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    static int findDuplicate(int[] arr){
+        int i = 0;
+        while (i < arr.length){
+
+            if (arr[i] != i+1){
+                int correct = arr[i] - 1;
+                if( arr[i] != arr[correct]){
+                    swap(arr, i, correct );
+                }else{
+                    return arr[i];
+                }
+            }
+            else{
+                i++;
+            }
+        }
+        return -1;
+    }
+    static void swap(int[] arr, int first ,int second){
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+}
